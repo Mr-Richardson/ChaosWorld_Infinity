e1883283169e73f662523ca40882a2e9df8dae59
Mr_Richardson <mr.richardson@outlook.de>
2026-06-01 16:03:04 +0200

    refactor(Camera): remove confusing translation and scaling

--- src/gameplay/Camera.kt
+++ src/gameplay/Camera.kt
@@ -9,7 +9,5 @@ class Camera(private val p: PApplet, private val settings: Settings, private var
     fun move(location: Float) {
         velocity = pos + location - p.width * settings.cameraFocus
         pos -= velocity * settings.cameraSmoothness
-        p.translate(pos, p.height.toFloat())
-        p.scale(1f, -1f)
     }
 }
