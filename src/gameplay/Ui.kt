d95fa8c0b380014376a3432779c56712638132c9
Mr_Richardson <mr.richardson@outlook.de>
2026-06-02 19:01:59 +0200

    fix(Ui): fix upside down UI display

--- src/gameplay/Ui.kt
+++ src/gameplay/Ui.kt
@@ -5,18 +5,24 @@ import p
 import processing.core.PApplet
 
 class Ui(private val objectManager: ObjectManager) {
-    fun render(seed: Long, score: Int) { // TODO: fix upside down
-        p.fill(255)
-        p.textAlign(PApplet.LEFT, PApplet.BOTTOM)
-        p.textSize(13f * objectManager.settings.uiScale)
+    init {
         objectManager.font.apply()
+    }
+
+    fun render(seed: Long, score: Int) {
+        p.resetMatrix()
+        p.fill(255)
+        // seed rendering
+        p.textAlign(PApplet.LEFT, PApplet.BOTTOM)
+        p.textSize(12f * objectManager.settings.uiScale)
         p.text(
             seed.toString(),
             1f * objectManager.settings.uiScale,
             (p.height - 1).toFloat() * objectManager.settings.uiScale
-        ) //  seed printing
+        )
+        // score rendering
         p.textAlign(PApplet.LEFT, PApplet.TOP)
-        p.textSize(0.03f * objectManager.settings.uiScale)
-        p.text(score, 10 * objectManager.settings.uiScale, p.height * objectManager.settings.uiScale) //  distance printing
+        p.textSize(30f * objectManager.settings.uiScale)
+        p.text(score, 10 * objectManager.settings.uiScale, 10 * objectManager.settings.uiScale) //  distance printing
     }
 }
