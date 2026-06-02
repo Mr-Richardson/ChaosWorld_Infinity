fccc52c96216f07460d4dd38c1174a4941ca0186
Mr_Richardson <mr.richardson@outlook.de>
2026-06-02 19:04:29 +0200

    refactor(Difficulty): adjust score multiplier

--- src/gameplay/Difficulty.kt
+++ src/gameplay/Difficulty.kt
@@ -4,7 +4,7 @@ import kotlin.math.max
 
 class Difficulty(var score: Int) {
     fun updateScore(pos: Double) {
-        score = max(score, pos.toInt())
+        score = max(score, pos.toInt() * 100)
     }
 }
 // Difficulty levels and increasing difficulty during gameplay
