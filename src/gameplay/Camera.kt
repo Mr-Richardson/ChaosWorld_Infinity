95b61441bb9325c5765f87c5127bc6cb5770569f
Mr_Richardson <mr.richardson@outlook.de>
2026-06-04 11:01:27 +0200

    fix(Camera): camera when zooming

--- src/gameplay/Camera.kt
+++ src/gameplay/Camera.kt
@@ -3,13 +3,12 @@ package gameplay
 import menu.Settings
 import p
 
-class Camera(private val settings: Settings, private var pos: Double) {
+class Camera(private val settings: Settings, var pos: Double) {
     private var velocity = 0f
 
     fun move(location: Double) {
-        //velocity = location - pos + settings.cameraFocus TODO: verify
-        pos += velocity * settings.cameraSmoothness
-        p.translate((pos * p.width).toFloat(), p.height.toFloat())
+        pos += (location - settings.cameraFocus - pos) * settings.cameraSmoothness
         p.scale(settings.zoom, -settings.zoom)
+        p.translate((-pos * p.width).toFloat(), (p.height / settings.zoom * -0.9f))
     }
 }
