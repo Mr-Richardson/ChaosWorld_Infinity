da655230d3038ab1eebd807eb123b78acc70d37f
Mr_Richardson <mr.richardson@outlook.de>
2026-06-04 14:16:42 +0200

    refactor(Character): allow player atributs to be changed mid-game

--- src/gameplay/Character.kt
+++ src/gameplay/Character.kt
@@ -7,11 +7,6 @@ import p
 import processing.core.PApplet
 
 class Character(private val obstacles: ObstacleManager, private val key: Key, private val settings: Settings) {
-    private val radius = settings.playerRadius
-    private val maxJump = settings.playerMaxJump
-    private val friction = settings.friction
-    private val epsilon = settings.epsilon
-    private val maxSpeed = settings.playerMaxSpeed
     val pos = settings.playerStart.copy()
     private val vel = Vector(0.0, 0.0)
     private var canJump = true
@@ -63,33 +58,32 @@ class Character(private val obstacles: ObstacleManager, private val key: Key, pr
     fun collisionCheck() {    //TODO: time-based movement processioning
         if (vel.x > epsilon) { //  forward
             for (o in obstacles.obstacle) {
-                if (o.y1 <= pos.y + radius && pos.y - radius <= o.y2 && o.x1 - pos.x - radius < vel.x && o.x1 > pos.x + radius) {
-                    vel.x = o.x1 - pos.x - radius - epsilon
+                if (o.y1 < pos.y + settings.playerRadius && pos.y - settings.playerRadius < o.y2 && o.x1 - pos.x - settings.playerRadius < vel.x && o.x1 > pos.x + settings.playerRadius) {
+                    vel.x = o.x1 - pos.x - settings.playerRadius - settings.epsilon
                 }
             }
-        } else if (vel.x < -epsilon) { // backward
+        } else if (vel.x < -settings.epsilon) { // backward
             for (o in obstacles.obstacle) {
-                if (o.y1 <= pos.y + radius && pos.y - radius <= o.y2 && o.x2 - pos.x + radius > vel.x && o.x2 < pos.x - radius) {
-                    vel.x = o.x2 - pos.x + radius + epsilon
+                if (o.y1 < pos.y + settings.playerRadius && pos.y - settings.playerRadius < o.y2 && o.x2 - pos.x + settings.playerRadius > vel.x && o.x2 < pos.x - settings.playerRadius) {
+                    vel.x = o.x2 - pos.x + settings.playerRadius + settings.epsilon
                 }
             }
         }
         //  hitbox check (y)
-        if (vel.y > epsilon) { //  upward
+        if (vel.y > settings.epsilon) { //  upward
             for (o in obstacles.obstacle) {
-                if (o.x1 < pos.x + radius && pos.x - radius < o.x2 && o.y1 - pos.y - radius < vel.y && o.y1 > pos.y + radius) {
-                    vel.y = o.y1 - pos.y - radius - epsilon
+                if (o.x1 < pos.x + settings.playerRadius && pos.x - settings.playerRadius < o.x2 && o.y1 - pos.y - settings.playerRadius < vel.y && o.y1 > pos.y + settings.playerRadius) {
+                    vel.y = o.y1 - pos.y - settings.playerRadius - settings.epsilon
                 }
             }
         } else { // downward
-            if (vel.y < -epsilon) {
+            if (vel.y < -settings.epsilon) {
                 for (o in obstacles.obstacle) {
-                    if (o.x1 < pos.x + radius && pos.x - radius < o.x2 && o.y2 - pos.y + radius > vel.y && o.y2 < pos.y - radius) {
-                        vel.y = o.y2 - pos.y + radius + epsilon
+                    if (o.x1 < pos.x + settings.playerRadius && pos.x - settings.playerRadius < o.x2 && o.y2 - pos.y + settings.playerRadius > vel.y && o.y2 < pos.y - settings.playerRadius) {
+                        vel.y = o.y2 - pos.y + settings.playerRadius + settings.epsilon
+                        canJump = true
                     }
                 }
-            } else {
-                canJump = true
             }
         }
     }
